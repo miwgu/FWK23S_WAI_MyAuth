f6f2b336d812adbf041395317700d6ed0e5e7f9a
Miwa G <miwa.guhres@hotmail.com>
2024-11-12 10:39:55 +0100

    update DB

--- lia_db2024.sql
+++ lia_db2024.sql
@@ -1,15 +1,20 @@
-drop database if exists lia_db2024;
-create database lia_db2024;
+drop database if exists lia_db2024; --check docker dbname
+create database lia_db2024; 
 use lia_db2024;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
-);
+    role ENUM('admin', 'user') NOT NULL,
+    avatar VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
 
 -- Insert some sample data
-INSERT INTO users (email, password, role) VALUES 
-('admin@example.com', '1234', 'admin'),
-('user1@example.com', '2345', 'user');
+--adminpass:1234  user1:2345
+INSERT INTO users (email, username, password, role, avatar) VALUES
+('admin@example.com', 'admin', '$2b$10$918OeDGV.rVck6il11t7Jeo.RM8aCExZVh6QRJkRn9z9Wyti7uiNS', 'admin', 'https://i.pravatar.cc/100?img=16'),
+('user1@example.com', 'user1', '$2b$10$Sya2OTdEa4gir6/mnb3lt.f2cgnw6JyBq8MCmg.isxMAsTKu0ZaqC', 'user', 'https://i.pravatar.cc/100?img=5');
